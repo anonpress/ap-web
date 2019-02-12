22642c59175688d244b3834910748b955d7bc83c
tortoise <sdgillen@crimson.ua.edu>
2019-02-11 19:55:39 -0600

    do not load menu asynchronously

--- anonpress.org/includes/ace-responsive-menu.php
+++ anonpress.org/includes/ace-responsive-menu.php
@@ -1,4 +1,4 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <?php require_once('jquery.php'); ?>
-<script src="/lib/ace-responsive-menu/ace-responsive-menu-min.js" async></script>
+<script src="/lib/ace-responsive-menu/ace-responsive-menu-min.js"></script>
 <link rel="stylesheet" href="/lib/ace-responsive-menu/ace-responsive-menu.css">
