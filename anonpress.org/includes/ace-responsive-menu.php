c502e362606936281d899c7ddf77320c58444ef0
Sean Gillen <sdgillen@crimson.ua.edu>
2020-04-01 01:19:58 -0500

    force scripts to run in correct order on store

--- anonpress.org/includes/ace-responsive-menu.php
+++ anonpress.org/includes/ace-responsive-menu.php
@@ -1,4 +1,4 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <?php require_once('jquery.php'); ?>
-<script src="//anonpress.org/lib/ace-responsive-menu/ace-responsive-menu-min.js"></script>
-<link rel="stylesheet" href="//anonpress.org/lib/ace-responsive-menu/ace-responsive-menu-min.css">
+<script id="menu-script" src="//anonpress.org/lib/ace-responsive-menu/ace-responsive-menu-min.js"></script>
+<link rel="stylesheet" href="//anonpress.org/lib/ace-responsive-menu/ace-responsive-menu.min.css?v=202004010109">
