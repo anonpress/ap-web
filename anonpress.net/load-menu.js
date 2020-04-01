1a6a0af3ddb6a8468cb962f22695a2216e2425fc
Sean Gillen <sdgillen@crimson.ua.edu>
2020-04-01 01:52:16 -0500

    longer loop and return on failure

--- anonpress.net/load-menu.js
+++ anonpress.net/load-menu.js
@@ -1,7 +1,11 @@
 $("nav#menubar").load("https://anonpress.org/includes/nav.php");
 $.getScript($("#menu-script").attr("src"), async function() {
-	for (let i = 10; i < 1000, !$.isFunction($("ace").aceResponsiveMenu); i += 10) {
+	for (let i = 10; i < 5000, !$.isFunction($("ace").aceResponsiveMenu); i += 10) {
 		await new Promise(r => setTimeout(r, i)); // Give ace some time to sort itself out
+	}
+	if (!$.isFunction($("ace").aceResponsiveMenu)) {
+		console.error("Failed to load menu for some reason :(");
+		return; // give up
 	}
 	$.globalEval($("nav#menubar script:not([src])").html());
 	$("#top-nav").css("display", "none");
