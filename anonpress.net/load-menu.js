c30ca18b7ae69cb3bf24990175da4723c70a739f
Sean Gillen <sdgillen@crimson.ua.edu>
2020-04-01 02:46:12 -0500

    additional logging

--- anonpress.net/load-menu.js
+++ anonpress.net/load-menu.js
@@ -1,4 +1,5 @@
 $("nav#menubar").load("https://anonpress.org/includes/nav.php");
+console.log("Attempting to load menu...");
 $.getScript($("#menu-script").attr("src"), async function() {
 	for (let i = 10; i < 5000, !$.isFunction($("ace").aceResponsiveMenu); i += 10) {
 		await new Promise(r => setTimeout(r, i)); // Give ace some time to sort itself out
@@ -8,7 +9,10 @@ $.getScript($("#menu-script").attr("src"), async function() {
 		return; // give up
 	}
 	console.log("Initializing menu here...");
-	$.globalEval($("nav#menubar script:not([src])").html());
+	$("nav#menubar script:not([src])").each(function() {
+		$.globalEval($(this).html());
+	});
 	$("#top-nav").css("display", "none");
 	$("nav#menubar").css("display", "");
-});
+}).fail(console.log("Failed to load menu :("));
+console.log("Done loading menu");
