d7f7d19ce5f790b19db49b2f1f6b4f00cec189ca
Sean Gillen <sdgillen@crimson.ua.edu>
2020-04-01 00:04:59 -0500

    minor refactoring

--- anonpress.org/index.php
+++ anonpress.org/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 chdir('lib/littlepoll');
 include('lib/littlepoll/lp_source.php');
 chdir('../..') ?>
@@ -9,15 +9,20 @@ chdir('../..') ?>
 <meta name="description" content="Alcoholics Anonymous Books, Software, Phone Numbers, FAQ & AA Big Book Indexes">
 <meta name="robots" content="index,follow">
 <meta name="viewport" content="width=400, initial-scale=1">
-<style>
-#randquote>h2:after,.colon:after,div.flex>section>h2:after{content:':'}h1{margin-left:5%;max-width:90%}header form{margin-right:5%}button{padding:.5em 1em;font:inherit;border-radius:9px;border:1px solid #ccc;color:#333;background:linear-gradient(to bottom,#fff 0,#f2f2f2 50%)}header button{font-size:18pt}button:hover{cursor:pointer;color:#33f}button:active{background:linear-gradient(to top,#fff 0,#f2f2f2 50%)}body>section{margin-left:10%;margin-right:10%}#randquote{max-width:615px}#randquote>h2{font-size:large;color:#808040;margin:0;font-weight:inherit}#randquote blockquote{margin:0 1.5em}#randquote p{margin:0}#randquote cite{font-size:small;color:#666;font-style:normal;margin-left:.5em}.flex{display:flex;justify-content:space-evenly;flex-wrap:wrap;align-items:flex-start}.shadowbox,div.flex>section{border:thin outset #ccc;border-radius:.5em;box-shadow:2px 2px 5px #333;padding:.5em;margin:2px 5px 5px 2px;position:relative;background-color:rgba(255,255,255,187)}div.flex>section{min-width:350px;position:relative;margin-top:36pt;padding:1em}div.flex>section>h2{position:absolute;top:-26pt;left:0;font-size:24pt;text-transform:uppercase;margin:0;color:#808040;font-weight:200}address{font-feature-settings:"lnum";font-style:normal}@media screen and (max-width:560px){div.optional.flex{display:block;text-align:center}}ul.polls :not(h4)>a{color:#666}ul.polls>li>ul>li{margin-bottom:1em}ul.links li.center{text-align:center;letter-spacing:.06em}.def{display:inline-block;width:auto}.def:first-letter{font-weight:700}@media screen and (max-width:840px){#randquote{margin:0 1em}}
-</style>
-<?php require_once('includes/style.php'); include_once('includes/tooltip.php'); require_once('includes/mailto.php'); include_once('includes/analytics.php');
-	require_once('includes/nav.php'); ?>
+<?php
+require_once 'index.min.css.html';
+require_once 'includes/style.php';
+include_once 'includes/tooltip.php';
+require_once 'includes/mailto.php';
+include_once 'includes/analytics.php';
+require_once 'includes/nav.php';
+?>
 <header>
 	<div class="flex" style="justify-content: space-between; align-items: center;">
 		<h1><img src="/assets/The-Anonymous-Press-green.gif" alt="The Anonymous Press" width="394" height="79"></h1>
-		<form data-email><button>Contact Us</button></form>
+		<form data-email>
+			<button>Contact Us</button>
+		</form>
 	</div>
 </header>
 <section id="randquote">
@@ -51,7 +56,8 @@ chdir('../..') ?>
 				</ul>
 			<li><h4 class="weak"><a href="/faq">AA Questions &amp; FAQ</a></h4>
 				<ul>
-					<li><span class="def">Frequently</span> <span class="def">Asked</span> <span class="def">Questions</span> about AA
+					<li><span class="def">Frequently</span> <span class="def">Asked</span> <span
+							class="def">Questions</span> about AA
 				</ul>
 			<li><h4 class="weak"><a href="/translate">Translation project</a></h4>
 				<ul>
@@ -63,7 +69,8 @@ chdir('../..') ?>
 				</ul>
 			<li><h4 class="weak"><a href="/spencer">The Spencer Misquotation</a></h4>
 				<ul>
-					<li class="small">The research proving Herbert Spencer<br>did not write the quote about avoiding<br>"contempt prior to investigation"
+					<li class="small">The research proving Herbert Spencer<br>did not write the quote about avoiding<br>"contempt
+						prior to investigation"
 				</ul>
 		</ul>
 	</section>
@@ -71,23 +78,26 @@ chdir('../..') ?>
 		<h2>Downloads</h2>
 		<ul class="links">
 			<li><h4><a href="/choices">The text of <i>Alcoholics Anonymous</i></a></h4>
-				<ul><li>Available for:
-					<ul>
-						<li><a href="/choices">Windows</a>
-						<li><a href="/choices/mac.htm">Mac</a>
-						<li><a href="/choices/android.htm">Android</a>
-						<li><a href="/choices/iphone.htm">iPhone</a>
-							<span class="spacer">/</span>
-							<a href="/choices/ipad.htm">iPad</a>
-						<li><a href="/choices/windowsmobile.htm">Windows Phone</a>
-						<li><a href="/choices/blackberry.htm">Blackberry</a>
-						<li><a href="/choices/kindle.htm">Kindle</a>
-						<li><a href="/choices/epub.htm">ePub</a>
-						<li style="padding-left: 1em;"><a class="strong" href="/choices">&amp; more…</a>
-					</ul>
+				<ul>
+					<li>Available for:
+						<ul>
+							<li><a href="/choices">Windows</a>
+							<li><a href="/choices/mac.htm">Mac</a>
+							<li><a href="/choices/android.htm">Android</a>
+							<li><a href="/choices/iphone.htm">iPhone</a>
+								<span class="spacer">/</span>
+								<a href="/choices/ipad.htm">iPad</a>
+							<li><a href="/choices/windowsmobile.htm">Windows Phone</a>
+							<li><a href="/choices/blackberry.htm">Blackberry</a>
+							<li><a href="/choices/kindle.htm">Kindle</a>
+							<li><a href="/choices/epub.htm">ePub</a>
+							<li style="padding-left: 1em;"><a class="strong" href="/choices">&amp; more…</a>
+						</ul>
 				</ul>
 			<li><h4 class="weak"><a href="/sobrietycheck">SobrietyCheck</a></h4>
-				<ul><li class="small">Sobriety counter for Windows</ul>
+				<ul>
+					<li class="small">Sobriety counter for Windows
+				</ul>
 			<li><h4 class="weak">Windows Desktop <a href="/quotegadget">Quote Gadget</a></h4>
 			<li><h4 class="weak"><a href="/bbindex">Big Book<sup>®</sup> Index</a></h4>
 			<li><h4 class="weak"><a href="/vore">The Varieties of Religious Experience</a></h4>
@@ -100,9 +110,15 @@ chdir('../..') ?>
 				<?=$question?>
 			</div>
 			<div style="margin-left: 77px;">
-				<?php if($votingstep==1) { echo($step1str); } 
-					if($votingstep==2) { echo($step2str); } 
-					if($votingstep==3) { echo($step3str); }
+				<?php if ($votingstep == 1) {
+					echo($step1str);
+				}
+				if ($votingstep == 2) {
+					echo($step2str);
+				}
+				if ($votingstep == 3) {
+					echo($step3str);
+				}
 				?></form> <!-- end tag for unclosed form generated by littlepoll -->
 			</div>
 			<div style="text-align: right; padding-right: 8px;">
@@ -112,15 +128,22 @@ chdir('../..') ?>
 			<ul class="links polls">
 				<li><h4 class="weak"><a class="colon" href="/polls/results.htm">Previous polls</a></h4>
 					<ul>
-						<li><a href="polls/results.htm#24" title="Very Optimistic…">Do you think you will drink again?</a>
-						<li><a href="polls/results.htm#19" title="Probably…">Do you think believe newcomers should avoid dating?</a>
-						<li><a href="polls/results.htm#17" title="We disagree…">Do you think drug addiction and alcoholism are the same thing?</a>
-						<li><a href="polls/results.htm#23" title="Yeah, probably…">Is the next drink a choice if you have been sober a while? </a>
-						<li><a href="polls/results.htm#1" title="Most people think it is OK…">Is it appropriate to use the Lord's Prayer in meetings?</a>
+						<li><a href="polls/results.htm#24" title="Very Optimistic…">Do you think you will drink
+								again?</a>
+						<li><a href="polls/results.htm#19" title="Probably…">Do you think believe newcomers should avoid
+								dating?</a>
+						<li><a href="polls/results.htm#17" title="We disagree…">Do you think drug addiction and
+								alcoholism are the same thing?</a>
+						<li><a href="polls/results.htm#23" title="Yeah, probably…">Is the next drink a choice if you
+								have been sober a while? </a>
+						<li><a href="polls/results.htm#1" title="Most people think it is OK…">Is it appropriate to use
+								the Lord's Prayer in meetings?</a>
 					</ul>
 				<li class="center"><a href="/polls/results.htm">More poll results...</a>
 				<li class="center"><a href="/polls/question.php">Suggest a poll question</a>
-				<li class="center"><a title="Sometimes we get curious about how the fellowship beyond our homegroup feels about stuff. Since we get AA folks visiting from all over we thought we might try getting a sense for how people feel about a few different things. The results are not scientific but we do think they give a rough idea of the sentiment of the fellowship.">About this poll</a>
+				<li class="center"><a
+						title="Sometimes we get curious about how the fellowship beyond our homegroup feels about stuff. Since we get AA folks visiting from all over we thought we might try getting a sense for how people feel about a few different things. The results are not scientific but we do think they give a rough idea of the sentiment of the fellowship.">About
+						this poll</a>
 			</ul>
 		</div>
 	</section>
@@ -129,11 +152,17 @@ chdir('../..') ?>
 		<div class="optional flex" style="justify-content: space-between; align-items: center;">
 			<ul class="links" style="max-width: 35em; margin-right: 2em;">
 				<li><h4 class="weak" style="margin: 0;"><a href="/info/local_meetings.htm">Meeting information</a></h4>
-					<ul><li class="small">How to find an AA meeting.</li></ul>
+					<ul>
+						<li class="small">How to find an AA meeting.</li>
+					</ul>
 				<li><h4 class="weak"><a href="/faq">AA FAQ</a></h4>
-					<ul><li class="small">Answers to most of the questions<br>we are asked about AA.</ul>
+					<ul>
+						<li class="small">Answers to most of the questions<br>we are asked about AA.
+					</ul>
 				<li><h4 class="weak"><a href="/info/statemen.htm">About us</a></h4>
-					<ul><li class="small">Includes a FAQ about us.</ul>
+					<ul>
+						<li class="small">Includes a FAQ about us.
+					</ul>
 				<li><h4 class="weak"><a href="/info/returns.htm">Refund Policy</a></h4>
 				<li><h4 class="weak"><a href="/info/privacy.htm">Privacy &amp; Security Statement</a></h4>
 			</ul>
@@ -145,7 +174,8 @@ chdir('../..') ?>
 					<span class="small">fax (509)&nbsp;779-4339</span><br>
 					<a data-email></a>
 				</address>
-				<p style="margin-bottom: 0;"><strong style="color: #900;">Please</strong><span class="small"> do <em style="color: red;">not</em> write with<br>questions about meetings</span><br>
+				<p style="margin-bottom: 0;"><strong style="color: #900;">Please</strong><span class="small"> do <em
+							style="color: red;">not</em> write with<br>questions about meetings</span><br>
 					<a class="strong" href="/info/local_meetings.htm">click here for meeting info</a>
 			</div>
 		</div>
@@ -165,8 +195,10 @@ chdir('../..') ?>
 		<li>Made a list of all persons we had harmed, and became willing to make amends to them all.
 		<li>Made direct amends to such people wherever possible, except when to do so would injure them or others.
 		<li>Continued to take personal inventory and when we were wrong promptly admitted it.
-		<li>Sought through prayer and meditation to improve our conscious contact with God as we understood Him, praying only for knowledge of His will for us and the power to carry that out.
-		<li>Having had a spiritual awakening as the result of these steps, we tried to carry this message to alcoholics, and to practice these principles in all our affairs.
+		<li>Sought through prayer and meditation to improve our conscious contact with God as we understood Him, praying
+			only for knowledge of His will for us and the power to carry that out.
+		<li>Having had a spiritual awakening as the result of these steps, we tried to carry this message to alcoholics,
+			and to practice these principles in all our affairs.
 	</ol>
 </section>
 <footer>
