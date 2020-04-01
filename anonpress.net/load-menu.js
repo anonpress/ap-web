a67c00e1559c2348de717fdf4b6b6382156e0e4f
Sean Gillen <sdgillen@crimson.ua.edu>
2020-04-01 01:47:05 -0500

    use set timeout for ace init

--- anonpress.net/load-menu.js
+++ anonpress.net/load-menu.js
@@ -1,6 +1,8 @@
 $("nav#menubar").load("https://anonpress.org/includes/nav.php");
-$.getScript($("#menu-script").attr("src"), function() {
-	while (!$.isFunction($("#ace").aceResponsiveMenu)) {} // Wait for aceResponsiveMenu to figure out how it works
+$.getScript($("#menu-script").attr("src"), async function() {
+	if (!$.isFunction($("ace").aceResponsiveMenu)) {
+		await new Promise(r => setTimeout(r, 100)); // Give ace 100ms to sort itself out
+	}
 	$.globalEval($("nav#menubar script:not([src])").html());
 	$("#top-nav").css("display", "none");
 	$("nav#menubar").css("display", "");
