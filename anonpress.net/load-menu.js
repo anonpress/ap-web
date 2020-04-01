add79e22463dee8dc7f3271f9390468709fc0aa9
Sean Gillen <sdgillen@crimson.ua.edu>
2020-04-01 03:07:21 -0500

    load script synchronously

--- anonpress.net/load-menu.js
+++ anonpress.net/load-menu.js
@@ -1,18 +1,24 @@
 $("nav#menubar").load("https://anonpress.org/includes/nav.php");
 console.log("Attempting to load menu...");
-$.getScript($("#menu-script").attr("src"), async function() {
-	for (let i = 10; i < 5000, !$.isFunction($("ace").aceResponsiveMenu); i += 10) {
-		await new Promise(r => setTimeout(r, i)); // Give ace some time to sort itself out
+$.ajax({
+	url: $("#menu-script").attr("src"),
+	dataType: 'script',
+	async: false,
+	cache: true,
+	success: async function() {
+		for (let i = 10; i < 5000, !$.isFunction($("ace").aceResponsiveMenu); i += 10) {
+			await new Promise(r => setTimeout(r, i)); // Give ace some time to sort itself out
+		}
+		if (!$.isFunction($("ace").aceResponsiveMenu)) {
+			console.error("Failed to load menu for some reason :(");
+			return; // give up
+		}
+		console.log("Initializing menu here...");
+		$("nav#menubar script:not([src])").each(function() {
+			$.globalEval($(this).html());
+		});
+		$("#top-nav").css("display", "none");
+		$("nav#menubar").css("display", "");
+		$("head").append("<style>@media screen and (max-width: 965px) { header { display: none; } }</style>");
 	}
-	if (!$.isFunction($("ace").aceResponsiveMenu)) {
-		console.error("Failed to load menu for some reason :(");
-		return; // give up
-	}
-	console.log("Initializing menu here...");
-	$("nav#menubar script:not([src])").each(function() {
-		$.globalEval($(this).html());
-	});
-	$("#top-nav").css("display", "none");
-	$("nav#menubar").css("display", "");
-}).fail(console.log("Failed to load menu :("));
-console.log("Done loading menu");
+}).done(console.log("Done loading menu :)")).fail(console.log("Failed to load menu :("));
