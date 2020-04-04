1a621f3a05d444c83ad25d463bc8f9105a8a6662
Sean Gillen <sdgillen@crimson.ua.edu>
2020-04-03 22:06:53 -0500

    remove some stray code from load-menu.js

--- anonpress.net/load-menu.js
+++ anonpress.net/load-menu.js
@@ -4,6 +4,5 @@ $(function() {
 		$("#top-nav").css("display", "none");
 		$("nav#menubar").css("display", "");
 		$("head").append("<style>@media screen and(max-width:965px){header>*{display:none;}.page_header>.container{top:2em;}}</style>");
-		if ($("#ace").hasClass("hide-menu"))
 	});
 });
