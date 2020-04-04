c1c4afc06bc3150e9a29ae93c7fc2dea7d04797e
Sean Gillen <sdgillen@crimson.ua.edu>
2020-04-03 22:16:56 -0500

    correct css minification

--- anonpress.net/load-menu.js
+++ anonpress.net/load-menu.js
@@ -2,6 +2,6 @@ $(function() {
 	$.get("https://anonpress.org/includes/nav.php", function(navbar) {
 		$("nav#menubar").html(navbar);
 		$("#top-nav").css("display", "none");
-		$("head").append("<style>@media screen and(max-width:965px){header>*{display:none;}.page_header>.container{top:2em;}}</style>");
+		$("head").append("<style>@media screen and (max-width:965px){header>*{display:none}.page_header>.container{top:2em}}</style>");
 	});
 });
