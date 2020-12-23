5fe8530f5bf5d3d1c1f146e1c4993fdd691868a5
Sean Gillen <sdgillen@crimson.ua.edu>
2020-12-22 19:22:42 -0800

    Add JS to automatically select only ST option
    in load-menu.js for now because making another script is hard and I don't want to

--- anonpress.net/load-menu.js
+++ anonpress.net/load-menu.js
@@ -2,4 +2,11 @@ $(function() {
 	$.get("https://anonpress.org/includes/nav.php", function(navbar) {
 		$("nav#menubar").html(navbar);
 	});
+
+	// TODO: Move this somewhere more suitable
+	// If there is exactly one option on the study edition page, select it
+	labelList = document.querySelectorAll('div#divOptionsBlock div.opt-field>label.radio-format>label');
+	if (labelList.length == 1) {
+		labelList[0].onclick();
+	}
 });
